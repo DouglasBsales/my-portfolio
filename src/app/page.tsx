067efe14481859b0daa9d8250ef7a52c53b5d1f6
douglas sales <douglas-8545@hotmail.com>
2024-07-31 21:00:38 -0300

    feat: added components and elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { Header } from "@/components//Home/Header";
+import { EnvieUmaMensagem } from "@/components/EnvieUmaMensagem/EnvieUmaMensagem";
 import { Principal } from "@/components/Home/Principal";
+import { Portfolio } from "@/components/Portfolio/Portfolio";
+import { Servicos } from "@/components/Serviços/Servicos";
+import { SobreMim } from "@/components/SobreMim/SobreMin";
+import { TecnologiasUtilizadas } from "@/components/Tecnologias/TecnologiasUtilizadas";
 
 export default function Home() {
   return (
@@ -12,9 +17,23 @@ export default function Home() {
         </div>
       </div>
       <div className="w-full h-screen flex flex-col items-center">
-        <div className="max-w-[1440px] w-full  flex flex-col pt-[100px] ">
-            <Principal/>
+        <div className="max-w-[1440px] w-full  flex flex-col pt-[100px]">
+          <Principal />
+          <SobreMim />
         </div>
+        <TecnologiasUtilizadas />
+        <div className="max-w-[1440px] w-full  flex flex-col pt-[100px]">
+          <Portfolio />
+        </div>
+        <div className="flex flex-col items-center">
+          <div className="w-[37px] h-[37px] flex justify-center items-center border-4 border-blackPrimary rounded-full ">
+            <div className="w-[5px] h-[5px] bg-blackPrimary rounded-full"></div>
+          </div>
+        </div>
+        <Servicos />
+        <div className="max-w-[1440px] w-full flex flex-col">
+        <EnvieUmaMensagem />
+      </div>
       </div>
     </div>
   );
